9bf23557dfc59a31b3e29c828c147474b415ecba
Gai Isay <gaiisay@outlook.de>
2022-11-17 13:01:50 +0100

    add mui date and time picker

--- pages/events/add/index.js
+++ pages/events/add/index.js
@@ -1,34 +1,61 @@
 import styled from "styled-components";
 import Button from "../../../components/Button";
+import TextField from "@mui/material/TextField";
+import { de } from "date-fns/locale";
+import { DatePicker, LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { useState } from "react";
 
 function AddEvent() {
+  const [date, setDate] = useState(null);
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+
   return (
     <main>
       //back button
       <h1>New Event</h1>
-      <StyledForm>
-        <label htmlFor="input-event-name">Event Name *</label>
-        <input type="text" id="input-event-name" name="title" required />
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
+        <StyledForm>
+          <label htmlFor="input-event-name">Event Name *</label>
+          <input type="text" id="input-event-name" name="title" maxLength={50} required />
 
-        <label htmlFor="input-description">Description</label>
-        <textarea id="input-description" name="description" rows="5" />
+          <label htmlFor="input-description">Description</label>
+          <textarea id="input-description" name="description" rows="5" />
 
-        <label htmlFor="input-date">Date *</label>
-        <input type="text" id="input-date" name="date" required />
+          <DatePicker
+            label="Date *"
+            value={date}
+            onChange={(newDate) => {
+              setDate(newDate);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <TimePicker
+            label="Start Time *"
+            value={startTime}
+            onChange={(newTime) => {
+              setStartTime(newTime);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <TimePicker
+            label="End Time *"
+            value={endTime}
+            onChange={(newTime) => {
+              setEndTime(newTime);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
 
-        <label htmlFor="input-event-name">Start Time *</label>
-        <input type="text" id="input-start-time" name="startTime" required />
+          <label htmlFor="input-location">Location</label>
+          <input type="text" id="input-location" name="location" />
 
-        <label htmlFor="input-event-name">End Time *</label>
-        <input type="text" id="input-end-time" name="endTime" required />
-
-        <label htmlFor="input-location">Location</label>
-        <input type="text" id="input-location" name="location" />
-
-        <Button type="submit" variant="create">
-          Create
-        </Button>
-      </StyledForm>
+          <Button type="submit" variant="create">
+            Create
+          </Button>
+        </StyledForm>
+      </LocalizationProvider>
     </main>
   );
 }
